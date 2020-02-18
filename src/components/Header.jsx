e1dfaf375e82d9147711f5c496abbf0458005002
[Riseapps] Bogdan Zhmay <bogdan.zhmay@riseapps.com>
2020-02-18 15:44:14 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTMbm6JTUYc5DkzrzBffprtqBfNHAF7S0hsen4Igajjhrf3uuU4"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="profile-img">
+        <img
+          src="https://images.unsplash.com/photo-1541442510208-33bf9a34886f?ixlib=rb-1.2.1&w=1000&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
